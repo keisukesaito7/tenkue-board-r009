59389d207a4bd2486eb4630fb647d7719aede97f
yell <ginger.yell.1209@gmail.com>
2020-11-16 23:14:07 +0900

    routes.rbのresources :commentsからindex new showを除外

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root to: "posts#index"
-  resources :comments;
+  resources :comments, only: [:create, :edit, :update, :destroy]
 end
