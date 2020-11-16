4feff58a8a9a1cd26b8ddec808ba95e090731a66
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-16 19:31:17 +0900

    posts_controllerのcreateアクションを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,17 @@ class PostsController < ApplicationController
   end
 
   def create
-    
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "登録しました"
+      redirect_to root_path
+    else
+      render 'posts/new'
+    end
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:content).merge(user_id: current_user.id)
+    end
 end
