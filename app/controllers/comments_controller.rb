1cfd756b827a5a43d1a79b185861ec88fac8b060
yell <ginger.yell.1209@gmail.com>
2020-11-17 23:24:18 +0900

    comments_controller.rbのcreateアクションを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,13 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-
+    @comment = @post.comments.new(comment_params)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post.id)
+    else
+      flash.now[:notice] = 'コメントを(140文字以内で)入力してください。'
+      redirect_to post_path(@post.id)
+    end
   end
 end
