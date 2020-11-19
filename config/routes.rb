6f7f4cc01e3232f93a575826de73e2e7e57b4180
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-19 18:45:58 +0900

    [修正・追加]likes.destroyアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root to: "posts#index"
-  resources :posts, except: :index do
-    resources :likes, only: :create
-  end
+  resources :posts, except: :index
+  post 'posts/:post_id/likes', to: 'likes#create', as: 'post_likes'
+  delete 'posts/:post_id/likes', to: 'likes#destroy', as: 'post_like'
 end
