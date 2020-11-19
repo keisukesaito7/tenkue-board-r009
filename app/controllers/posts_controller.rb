5a04c060a8b695b782df7c47aab3a51c36562c2a
yell <ginger.yell.1209@gmail.com>
2020-11-19 20:38:13 +0900

    posts_controller.rbにbefore_action :set_postを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
