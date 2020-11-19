d6652d66ddaf90a4dda64cfe113b7d26b2cf1b9b
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-19 18:16:37 +0900

    いいねされたか判定するメソッドをPostモデルに追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   validates :content, presence: true, length: { maximum: 140 }
+
+  def already_liked?(user)
+    self.likes.exists?(user_id: user.id)
+  end
 end
