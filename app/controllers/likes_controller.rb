540b9cc067212dc516aac53c053e33b4bf515601
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-19 17:47:38 +0900

    likes.createアクションを実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,9 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    redirect_to root_path
+  end
+
 end
