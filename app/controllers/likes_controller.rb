71df90084aec1dc6f3878dc7b2416f01c45087f1
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-19 18:44:48 +0900

    likes.destroyアクションを実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,13 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
     like = Like.create(user_id: current_user.id, post_id: params[:post_id])
+    redirect_to root_path
+  end
+
+  def destroy
+    Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
     redirect_to root_path
   end
 
