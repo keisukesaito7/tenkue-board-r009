a5b289cced04e353c6ae888a41bcebc1a58c8fb2
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-20 19:28:50 +0900

    [修正]リダイレクト先を直前のページに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,12 @@ class LikesController < ApplicationController
 
   def create
     like = Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to root_path
+    redirect_back(fallback_location: post_path(params[:post_id]))
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
-    redirect_to root_path
+    redirect_back(fallback_location: post_path(params[:post_id]))
   end
 
 end
