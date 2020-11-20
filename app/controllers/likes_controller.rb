ba8d18bbf9b1d9bd51661efbf2fa7f0d0357e323
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-20 21:31:37 +0900

    [修正]likes_controllerのbefore_actionから余分なアクションを除外

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,5 @@
 class LikesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :authenticate_user!, only: :create
 
   def create
     like = Like.create(user_id: current_user.id, post_id: params[:post_id])
