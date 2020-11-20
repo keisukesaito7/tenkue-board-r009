06800f2cc16eef14f986bf1b39d1d7a104b09fc1
yell <ginger.yell.1209@gmail.com>
2020-11-21 00:51:46 +0900

    コメント投稿機能のエラーメッセージを修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to post_path(@post.id)
     else
-      flash.now[:notice] = 'コメントを(140文字以内で)入力してください。'
+      flash[:alert] = 'コメントを(140文字以内で)入力してください。'
       redirect_to post_path(@post.id)
     end
   end
