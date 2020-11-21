5b0cb1ee401e4e4fc7522734fcced270c809139e
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-21 22:33:20 +0900

    destroyアクションを追加&set_postに追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
@@ -31,6 +31,11 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
   end
 
   private
