f356aedd8b4cfc30c5760f636ae1448986779335
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-21 18:43:54 +0900

    [修正]likes_controllerにset_postを実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,21 @@
 class LikesController < ApplicationController
   before_action :authenticate_user!, only: :create
+  before_action :set_post
 
   def create
-    like = Like.create(user_id: current_user.id, post_id: params[:post_id])
-    redirect_back(fallback_location: post_path(params[:post_id]))
+    like = Like.create(user_id: current_user.id, post_id: @post.id)
+    redirect_back(fallback_location: post_path(@post))
   end
 
   def destroy
-    Like.find_by(user_id: current_user.id, post_id: params[:post_id]).destroy
-    redirect_back(fallback_location: post_path(params[:post_id]))
+    Like.find_by(user_id: current_user.id, post_id: @post).destroy
+    redirect_back(fallback_location: post_path(@post))
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
 end
