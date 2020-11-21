ad9427325cc9c15279d6703a9a12b9170ce6f7a6
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-21 22:13:01 +0900

    [修正]likesアクションへのルーティングをpostsにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
   root to: "posts#index"
-  resources :posts, except: :index
-  post 'posts/:post_id/likes', to: 'likes#create', as: 'post_likes'
-  delete 'posts/:post_id/likes', to: 'likes#destroy', as: 'post_like'
+  resources :posts, except: :index do
+    resource :likes, only: [:create, :destroy]
+  end
 end
