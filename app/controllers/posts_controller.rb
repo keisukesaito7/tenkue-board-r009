a9aaad679b0865445d79feaa2bfa41be72cc30e8
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-21 22:27:48 +0900

    updateアクションを編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,11 @@ class PostsController < ApplicationController
   end
   
   def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   private
