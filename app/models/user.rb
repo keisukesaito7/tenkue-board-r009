0b9fe5f426ef62774ef8e0ba177d97a9322b7808
aoi1019 <54698622+aoi1019@users.noreply.github.com>
2020-11-22 14:17:31 +0900

    [修正]dependent: :destroy削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
