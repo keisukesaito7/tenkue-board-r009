2ed73fb2658a70a41ad7b1a32fb18ba25a240dfc
yell <ginger.yell.1209@gmail.com>
2020-11-22 23:25:42 +0900

    user.rbのhas_many :commentsからdependent: :destroyを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments, dependent: :destroy
+  has_many :comments
   has_many :likes
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
