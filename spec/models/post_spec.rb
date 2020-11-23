bcbfce8ff9c78bcc509a1c6ed8d758c4f7966332
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 14:46:33 +0900

    [Post]新規投稿の正常系をテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '新規投稿' do
+    context '新規投稿ができるとき' do
+      it '全ての値が正しく入力されれば保存できる' do
+        expect(@post).to be_valid
+      end
+    end
+  end
 end
