74c182c87c2656f98972521d909168f549ede86f
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 13:54:33 +0900

    [User]新規登録の正常系をテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe '新規登録' do
+    context '新規登録ができるとき' do
+      it '全ての値が正しく入力されれば保存できる' do
+        expect(@user).to be_valid
+      end
+    end
+  end
 end
