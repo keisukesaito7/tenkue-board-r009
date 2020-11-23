1cf77539553330c3755931af19587186c20d1e18
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 13:53:29 +0900

    [User]FactoryBotでユーザーを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name                  { Faker::Internet.username }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 8, max_length: 32) }
+    password_confirmation { password }
   end
 end
