cfd872a27f88b10278e317a714f6f4511605d21c
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 14:52:01 +0900

    [Post]新規投稿の異常系をテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,5 +11,23 @@ RSpec.describe Post, type: :model do
         expect(@post).to be_valid
       end
     end
+
+    context '新規投稿ができないとき' do
+      it 'contentが空だと保存できない' do
+        @post.content = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("投稿内容を入力してください")
+      end
+      it 'contentが141文字以上だと保存できない' do
+        @post.content = Faker::Lorem.characters(number: 141)
+        @post.valid?
+        expect(@post.errors.full_messages).to include("投稿内容は140文字以内で入力してください")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Userを入力してください")
+      end
+    end
   end
 end
