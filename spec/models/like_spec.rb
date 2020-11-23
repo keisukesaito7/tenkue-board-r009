e4041dd8ddd6a1ad0c8f837b69c2fb76ca1871ac
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 16:19:46 +0900

    [like]いいねの正常系をテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+
+  describe 'いいね' do
+    context 'いいねができるとき' do
+      it 'まだいいねしていない投稿にはいいねができる' do
+        expect(@like).to be_valid
+      end
+    end
+  end
 end
