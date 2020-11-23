25c832fbea22f7f09e235981d18c1cb6e9a46b7d
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-23 16:20:10 +0900

    [like]いいねの異常系をテスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -11,5 +11,26 @@ RSpec.describe Like, type: :model do
         expect(@like).to be_valid
       end
     end
+
+    context 'いいねができないとき' do
+      it 'ユーザーが紐付いていないと保存できない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Userを入力してください")
+      end
+      it '投稿が紐付いていないと保存できない' do
+        @like.post = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Postを入力してください")
+      end
+      it '既にいいねをした投稿にはいいねができない' do
+        @like.save
+        @like_repeat = FactoryBot.build(:like)
+        @like_repeat.user = @like.user
+        @like_repeat.post = @like.post
+        @like_repeat.valid?
+        expect(@like_repeat.errors.full_messages).to include("Postはすでに存在します")
+      end
+    end
   end
 end
