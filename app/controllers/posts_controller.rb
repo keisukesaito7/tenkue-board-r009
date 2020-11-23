b093fa38892de19eef27473f4db08c45ec3de61b
yell <ginger.yell.1209@gmail.com>
2020-11-23 16:33:50 +0900

    posts_controller.rbにskip_before_action :authenticate_user, only: [:index, :show]を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
