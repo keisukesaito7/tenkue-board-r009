b11ea0ee0950d0e4520fc54b173e4a2c1b4aaf83
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-24 18:50:00 +0900

    [comment]コメント投稿の異常系をテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -11,5 +11,28 @@ RSpec.describe Comment, type: :model do
         expect(@comment).to be_valid
       end
     end
+
+    context '新規投稿ができないとき' do
+      it 'commentが空だと保存できない' do
+        @comment.comment = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Commentを入力してください")
+      end
+      it 'commentが141文字以上だと保存できない' do
+        @comment.comment = Faker::Lorem.characters(number: 141)
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Commentは140文字以内で入力してください")
+      end
+      it 'userが紐付いていないと保存できない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'postが紐付いていないと保存できない' do
+        @comment.post = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("Postを入力してください")
+      end
+    end
   end
 end
