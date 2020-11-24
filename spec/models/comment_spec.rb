fa7faf5706795c5508a815f407221b99fd0632ea
keisukesaito7 <keisukesaito7@gmail.com>
2020-11-24 18:44:33 +0900

    [comment]コメント投稿の正常系をテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe '新規コメント投稿' do
+    context '新規投稿ができるとき' do
+      it '全ての値が正しく入力されれば保存できる' do
+        expect(@comment).to be_valid
+      end
+    end
+  end
 end
